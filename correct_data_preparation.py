657b62a579dc43fe2719ba0988a82ea9ae8bf6a0
sakairi-nobuyuki <SAKAIRI.Nobuyuki@gmail.com>
2020-08-05 00:48:25 +0900

    region detector inference by FRCNN

--- correct_data_preparation.py
+++ correct_data_preparation.py
@@ -4,6 +4,7 @@ import shutil
 import xmltodict
 import json
 import pprint
+import pathlib
 import xml.etree.ElementTree as ET
 
 def file_read(path):
@@ -31,6 +32,7 @@ def load_correct_data (correct_data_dir_path, path_str = None):
             dict = xmltodict.parse (file_read (xml_path))
             correct_sub_dict = {}
             correct_sub_dict['path'] = dict['annotation']['path']
+    
             annotation_list = []
             if 'object' in dict['annotation'].keys ():
                 if type (dict['annotation']['object']) != list:
@@ -46,4 +48,20 @@ def load_correct_data (correct_data_dir_path, path_str = None):
 
 if __name__ == '__main__':
     ### bouding_box = [n_bb, x1, y1, x2, y2, label]
-    load_correct_data ('annotation', path_str = 'jj')
+    correct_list = load_correct_data ('annotation', path_str = 'jj')
+    #    attr_list = []
+    #for correct_data in correct_list:
+    #    attr_list.extend[item['attr'] for item in correct_data['annotation']]
+    #attr_list = list (set (attr_list))
+    #print (attr_list)
+    #print (correct_list)
+
+    dataset_data = []
+    for correct_dict in correct_list:
+        pprint.pprint (correct_dict)
+        img_path = pathlib.Path (correct_dict['path']).relative_to (os.getcwd())
+        for bboxes in correct_dict['annotation']:
+            dataset_data.append (img_path, bboxes['x1'], bboxes['y1'], bboxes['x2'], bboxes['y2'], bboxes['attr'])
+    pprint.pprint (dataset_data)
+
+        
